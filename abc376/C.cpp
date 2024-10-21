4f6fcb092642144355470ece6bd51fdeac9d6aa7
riku546 <opa9788@gmail.com>
2024-10-21 21:13:28 +0900

    ２分探索

--- abc376/C.cpp
+++ abc376/C.cpp
@@ -4,15 +4,33 @@ using ll = long long;
 using ull = unsigned long long;
 using namespace std;
 
-int binary(vector<int>a , vector<int>b)
+bool judge(vector<int> a, vector<int> b, int mid)
 {
-  int r = 2e10 , l = 0;
-  while (r - l > 0)
+  
+  vector<int> cb = b;
+  cb.push_back(mid);
+  sort(cb.begin(), cb.end());
+  rep(i, 0, cb.size())
+  {
+    if (a[i] > cb[i])
+      return false;
+  }
+  return true;
+}
+
+int binary(vector<int> a, vector<int> b)
+{
+  int r = 2e9, l = 0;
+  while (r - l > 1)
   {
     int mid = l + (r - l) / 2;
-    
+    if (judge(a, b, mid))
+      r = mid;
+    else
+      l = mid;
   }
-  
+
+  return r;
 }
 
 int main()
@@ -23,5 +41,15 @@ int main()
   vector<int> b(n - 1);
   rep(i, 0, n) cin >> a[i];
   rep(i, 0, n - 1) cin >> b[i];
+  sort(a.begin(), a.end());
+  sort(b.begin(), b.begin());
 
+  int inf = 2e9;
+  if (!judge(a, b, inf))
+  {
+    cout << -1 << endl;
+    return 0;
+  }
+
+  cout << binary(a, b) << endl;
 }
